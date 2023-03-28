f4cd15a54b5c738c912270b7b5883d55871bcaef
abhijeetmohol <abhijeetmohol3101@gmail.com>
2023-03-28 20:47:52 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+// x,y,z are the variables using in following js.
+console.log("there is addition of two variables :");
+var x=50;
+var y=20;
+var z=x+y;
+console.log(z);
+console.log("above value is the sum of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is substraction of two variables :");
+var x=50;
+var y=20;
+var z=x-y;
+console.log(z);
+console.log("above value is the substraction of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is multiplication of two variables :");
+var x=50;
+var y=20;
+var z=x*y;
+console.log(z);
+console.log("above value is the multiplication of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is division of two variables :");
+var x=50;
+var y=20;
+var z=x/y;
+console.log(z);
+console.log("above value is the division of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is reminder of two variables :");
+var x=50;
+var y=20;
+var z=x%y;
+console.log(z);
+console.log("above value is the reminder of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is exponentiation of two variables :");
+var x=50;
+var y=20;
+var z=x**y;
+console.log(z);
+console.log("above value is the exponentiation of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is increment of two variables :");
+var x=50;
+var y=20;
+var z=++y;
+console.log(z);
+console.log("above value is the increment of 50 and 20.");
+
+// x,y,z are the variables using in following js.
+console.log("there is decrement of two variables :");
+var x=50;
+var y=20;
+var z=--y;
+console.log(z);
+console.log("above value is the decrement of 50 and 20.");
